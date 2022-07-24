5425799d3845075f2f82640e99d93260be326a7b
Olivia Roorbach <33472018+oroorbach@users.noreply.github.com>
2022-07-23 17:21:28 -0800

    Added current html, CSS and JS for weather app
    Working on this app before connecting to Git so adding in the first steps of the code to push through Git.

---
+++ src/index.js
@@ -0,0 +1,85 @@
+function formatTime(currentDate) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let localDay = days[currentDate.getDay()];
+  let localHr = currentDate.getHours();
+  let localMin = ("0" + currentDate.getMinutes()).slice(-2);
+  let localMonth = months[currentDate.getMonth()];
+  let localDate = currentDate.getDate();
+  let localYear = currentDate.getFullYear();
+
+  let formattedTime = `${localDay} ${localHr}:${localMin}`;
+  let formattedDate = `${localMonth} ${localDate}, ${localYear}`;
+  let time = document.querySelector("#localTime");
+  let daymonthyear = document.querySelector("#currentDate");
+  time.innerHTML = formattedTime;
+  daymonthyear.innerHTML = formattedDate;
+}
+let currentDate = new Date();
+formatTime(currentDate);
+
+function inputCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  document.querySelector("h2").innerHTML = city;
+  let apiKey = "c862a60ffa7620d74753b0a466bf96fa";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+
+  axios
+    .get(`${apiUrl}q=${city}&APPID=${apiKey}&units=imperial`)
+    .then(displayWeather);
+}
+
+document.querySelector("#city-info").addEventListener("submit", inputCity);
+
+function getLocation(event) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+  let apiKey = "c862a60ffa7620d74753b0a466bf96fa";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+
+  axios
+    .get(
+      `${apiUrl}lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=${apiKey}&units=imperial`
+    )
+    .then(displayWeather);
+}
+
+function displayWeather(response) {
+  document.querySelector(".actualDeg").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#weatherDes").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#feel").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("h2").innerHTML = response.data.name;
+}
+
+document.querySelector("#current-btn").addEventListener("click", getLocation);
